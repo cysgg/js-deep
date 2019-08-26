79489a1dd18778f90fe1d6b4129da994ca821705
cysgg <279709624@qq.com>
2019-08-26 21:05:39 +0800

    leetcode

--- leetcode/91.js
+++ leetcode/91.js
@@ -3,23 +3,28 @@
  * @return {number}
  */
 var numDecodings = function(s) {
-    let len = s.length
-    dp = Array.from({length:len},v=>1)
-    console.log(dp);
-    for(let i = 2; i <= len; i++){
-        const one = s.slice(i-1,i)
-        const two = s.slice(i-2,i)
-
-        if(one != 0){
-            dp[i] += dp[i-1]
-        }
-        if(two >= 10 && two <= 26){
-            dp[i] = dp[i-2]
-        }
+    if (s == null || s.length == 0) {
+      return 0;
     }
-    return dp[len-1]
-};
+    const dp = Array(s.length + 1).fill(0);
+    dp[0] = 1;
+    dp[1] = s[0] !== "0" ? 1 : 0;
+    for (let i = 2; i < s.length + 1; i++) {
+       const one = +s.slice(i - 1, i);
+       const two = +s.slice(i - 2, i);
+  
+      if (two >= 10 && two <= 26) {
+        dp[i] = dp[i - 2];
+      }
+  
+      if (one >= 1 && one <= 9) {
+        dp[i] += dp[i - 1];
+      }
+    }
+  
+    return dp[dp.length - 1];
+  };
 
 
-console.log(numDecodings('14'));
+console.log(numDecodings('226'));
 
