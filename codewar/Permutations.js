0744f26e5eaf33d99eea4499d220bb3fc2be965d
cysgg <279709624@qq.com>
2019-10-11 16:51:43 +0800

    codewar

--- codewar/Permutations.js
+++ codewar/Permutations.js
@@ -1,10 +1,20 @@
 function permutations(string) {
   let len = string.length;
   let arr = []
-  for (let i = 0; i < len; i++) {
-    for (let j = 0; j < len; j++) {
-      
+  fordeep(arr, len, string, '')
+  return arr
+}
+function fordeep (arr, len, string, temp) {
+  if (!string || string === '') {
+    !arr.includes(temp) && arr.push(temp)
+  } else {
+    for (let i = 0; i < string.length; i++) {
+      let temp1 = temp.slice()
+      let string1 = string.slice()
+      temp1 += string1[i]
+      string1 = string1.replace(string1[i], '')
+      fordeep(arr, len, string1, temp1)
     }
   }
 }
-console.log(permutations('ab'))
+console.log(permutations('aabb'))
