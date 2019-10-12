1e6fa47cdf5b13d55a332d3e4ea17634d43e68e7
cysgg <279709624@qq.com>
2019-10-12 21:08:52 +0800

    计数排序

--- algorithm/自己探索/排序算法.js
+++ algorithm/自己探索/排序算法.js
@@ -107,34 +107,63 @@ function guibing_sort (a, p, q, r) {
   }
 }
 
+function jishu (a) {
+  let max = Math.max(...a)
+  let min = Math.min(...a)
+  let b = Array.from({length:max-min+1}, v => 0)
+  console.log(b)
+  a.forEach(v => {
+    b[v-1]++
+  })
+  console.log(b)
+  b = b.reduce((p, v) => {
+    if (p.length === 0) {
+      p.push(v)
+    } else {
+      p.push(v + p[p.length - 1])
+    }
+    return p
+  }, [])
+  console.log(b)
+  let c = Array.from(a, v => 0)
+  console.log(c)
+  for (let i = 0; i < a.length; i++) {
+    c[(b[a[i] - 1]--) - 1] = a[i]
+  }
+  console.log(c)
+  return c
+}
 
 
-testArr = [4,5,6,1,2,3]
 
-console.time('maopao')
-console.log(maopao(testArr))
-console.timeEnd('maopao')
-console.log(testArr,'------------')
+testArr = [4,1,2,1,2,3,3,4,2,1,3,4,2,2,1,1,3,1,3,4,2]
 
-
-console.time('charu')
-console.log(charu(testArr))
-console.timeEnd('charu')
-console.log(testArr,'------------')
+// console.time('maopao')
+// console.log(maopao(testArr))
+// console.timeEnd('maopao')
+// console.log(testArr,'------------')
 
 
-console.time('maopao1')
-console.log(maopao(testArr))
-console.timeEnd('maopao1')
-console.log(testArr,'------------')
-
-console.time('kuaipai')
-console.log(kuaipai(testArr))
-console.timeEnd('kuaipai')
-console.log(testArr,'------------')
+// console.time('charu')
+// console.log(charu(testArr))
+// console.timeEnd('charu')
+// console.log(testArr,'------------')
 
 
-console.time('guibing')
-console.log(guibing(testArr))
-console.timeEnd('guibing')
-console.log(testArr,'------------')
+// console.time('maopao1')
+// console.log(maopao(testArr))
+// console.timeEnd('maopao1')
+// console.log(testArr,'------------')
+
+// console.time('kuaipai')
+// console.log(kuaipai(testArr))
+// console.timeEnd('kuaipai')
+// console.log(testArr,'------------')
+
+
+// console.time('guibing')
+// console.log(guibing(testArr))
+// console.timeEnd('guibing')
+// console.log(testArr,'------------')
+
+console.log(jishu(testArr));
