92a35ff95e5a2b9c931c3f0a1013b229f3990e51
cysgg <279709624@qq.com>
2019-11-06 10:18:27 +0800

    co 函数的实现

--- js-deep/es6/Generator.js
+++ js-deep/es6/Generator.js
@@ -1,3 +1,4 @@
+
 function wrapper (generatorFunc) {
   return function (...args) {
     let generatorObj = generatorFunc(...args)
@@ -46,3 +47,124 @@ function* fibonacci (num) {
 // }
 
 console.log(...fibonacci(10));
+
+function* g () {
+  yield 1
+  console.log('throwing an exception')
+  throw new Error('fenerator broke!')
+  yield 2
+  yield 3
+}
+
+function log(generator) {
+  var v
+  console.log('starting generator');
+  try {
+    v = generator.next()
+    console.log('第一次运行next方法', v)
+  } catch (error) {
+    console.log('捕捉错误', v)
+  }
+  try {
+    v = generator.next()
+    console.log('第二次运行next方法', v)
+  } catch (error) {
+    console.log('捕捉错误', v)
+  }
+  try {
+    v = generator.next()
+    console.log('第三次运行next方法', v)
+  } catch (error) {
+    console.log('捕捉错误', v)
+  }
+  console.log('ending generator');
+}
+log(g())
+
+// Generator.prototype.throw Generator.prototype.return
+
+
+function* numbers () {
+  yield 1
+  try {
+    yield 2
+    yield 3
+  } finally {
+    yield 4
+    yield 5
+  }
+  yield 6
+}
+
+let g1 = numbers()
+
+console.log(g1.next());
+console.log(g1.next());
+console.log(g1.return(7));
+console.log(g1.next());
+console.log(g1.next());
+console.log(g1.next());
+
+function* bar () {
+  yield 'a'
+  yield* fooo()
+  yield 'b'
+}
+
+function* fooo () {
+  yield 'x'
+  yield 'y'
+}
+
+console.log(...bar());
+
+function co (gen) {
+  var ctx = this
+  return new Promise((resolve, reject) => {
+    if (typeof gen === 'function') gen = gen.call(ctx)
+    if (!gen || typeof gen.next !== 'function') return resolve(gen)
+
+    onFulfilled()
+
+    function onFulfilled (res) {
+      var ret
+      try {
+        ret = gen.next(res)
+      } catch (error) {
+        return reject(error)
+      }
+      next(ret)
+    }
+
+    function onRejected (err) {
+      return reject(err)
+    }
+
+    function next(ret) {
+      console.log(ret)
+      if (ret.done) return resolve(ret.value)
+      if(ret.value && isPromise(ret.value)) {
+        console.log('then', ret.value)
+        ret.value.then(onFulfilled, onRejected)
+      } else {
+        console.log(ret.value, '------')
+        onRejected (new TypeError("error"))
+      }
+      
+    }
+  })
+  function isPromise(obj) {
+    return typeof obj === 'object' && typeof obj.then === 'function';
+  }
+}
+
+co(function* () {
+  let a = yield Promise.resolve(1)
+  let b = yield Promise.resolve(2)
+  console.log(a + b, '=====')
+  return a+b
+}).then(res => {
+  console.log('===', res)
+}).catch(err => {
+  console.log('===', err)
+})
--- js-deep/myTest/test.js
+++ js-deep/myTest/test.js
@@ -1,2 +1,62 @@
-let a = [1, 2, 3]
-console.log(a.slice(0, 12));
+// let n = 13
+// let dpList = [[[]]]
+// for (let i = 1; i <= n; i++) {
+//   getSum(i, 1, 2, 5, 10)
+// }
+
+// function getSum (index, o, t, f, ten) {
+//   dpList[index] = DuplicateRemoval([...getIndex(index ,o), ...getIndex(index ,t), ...getIndex(index ,f), ...getIndex(index ,ten)])
+// }
+
+// function getIndex (index, count) {
+//   if (index - count < 0) {
+//     return []
+//   }
+//   let arr = JSON.parse(JSON.stringify(dpList[index - count]))
+//   return arr.map(v => {
+//     v.push(count)
+//     return v
+//   })
+// }
+
+// function equalList (l1, l2) {
+//   let a1 = l1.slice()
+//   let a2 = l2.slice()
+//   a1.sort()
+//   a2.sort()
+//   if(a1.length !== a2.length) {
+//     return false
+//   }
+//   for (let i = 0; i < a1.length; i++) {
+//     if (a1[i] !== a2[i]) {
+//       return false
+//     }
+//   }
+//   return true
+// }
+
+// function DuplicateRemoval (list) {
+//   let result = list.reduce((p, n) => {
+//     if (p.length !== 0) {
+//       for (let i = 0; i < p.length; i++) {
+//         if (equalList(p[i], n)) {
+//           return p
+//         }
+//       }
+//       p.push(n)
+//     } else {
+//       p.push(n)
+//     }
+//     return p
+//   }, [])
+//   return result
+// }
+
+// console.log(dpList[n].length)
+
+let n = 13
+
+data = 1 + Math.floor(n / 2) + Math.floor(n / 5) + Math.floor(n / 10)
+
+
+console.log(data);
